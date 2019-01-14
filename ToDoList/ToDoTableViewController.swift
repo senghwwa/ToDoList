7d60a688e4b482b60a13a8805c4b54385816b83d
senghwwa <senghwabeh@hotmail.com>
2019-01-14 18:09:39 +0800

    Step 3 complete

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -15,6 +15,8 @@ class ToDoTableViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		navigationItem.leftBarButtonItem = editButtonItem
+		
 		if let savedToDos = ToDo.loadToDos() {
 			todos = savedToDos
 		} else {
@@ -36,4 +38,16 @@ class ToDoTableViewController: UITableViewController {
 		cell.textLabel?.text = todo.title
 		return cell
 	}
+	
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		return true
+	}
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			todos.remove(at: indexPath.row)
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		}
+	}
+	
 }
